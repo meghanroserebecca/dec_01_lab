b03b4b0afecca4e0b04101f8f46f78168691d1fe
William Madden <madden.william.d@gmail.com>
2016-12-01 16:34:40 -0800

    correct app file modified

--- conversion/appC.js
+++ conversion/appC.js
@@ -1,1 +1,20 @@
 'use strict';
+'use strict';
+var convertButton = document.getElementById('conver_button'); // varriable pointing at convert button HTML element
+convertButton.addEventListener ( click, conversionHandler);
+
+function inchesToCentemeter (input) {};
+function centimetersToTnches (input) {};
+function fluidOuncesToMilliliters (input) {};
+function millilitersToFluidOunces (input) {};
+function milesToKilometers (input) {};
+function kilometersToMiles (input) {};
+function poundsToKilograms (input) {};
+function kilogramsToPoundst (input) {};
+
+
+var arrayOfConversionFunction = [inchesToCentemeter, centimetersToTnches, fluidOuncesToMilliliters, millilitersToFluidOunces, , milesToKilometers, kilometersToMiles, poundsToKilograms, kilogramsToPounds];
+
+function conversionHandler (event){
+
+}
