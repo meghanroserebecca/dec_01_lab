a38c6e848e75ded69c35ccdbe1c9c73eff09e4e1
ForgottenSonata <forgottensonata@gmail.com>
2016-12-01 16:35:14 -0800

    updating files mara

--- conversion/appC.js
+++ conversion/appC.js
@@ -1,1 +1,17 @@
 'use strict';
+
+
+var x = document.CreateElement("ConversionCalculator");
+
+var userChoice;
+
+var userChoiceArray = ['inches to centimeters', 'centimeters to inches', 'ounces to mililiters', 'mililiters to ounces', 'miles to kilometers', 'kilometers to miles', 'pounds to kilograms ', 'kilograms to pounds'];
+
+function userChoice {
+ var selection = document.getElementById("convertor_select");
+ for(var i =0; i < userChoiceArray.length; i++){
+
+  if (selection === userChoiceArray[i]){
+  userChoice = i;
+  }
+}
